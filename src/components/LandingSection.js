73ef0e35246840eaa0c8e84f31dac4f27dd9d1f9
gizemayegul <gizemayazyegul@gmail.com>
2023-07-20 15:30:47 +0200

    homepage

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -19,7 +19,7 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365">
       
-      <Avatar size='4xs' src="https://media.licdn.com/dms/image/D4E03AQFJGw03IvpCJA/profile-displayphoto-shrink_400_400/0/1665585643902?e=1687996800&v=beta&t=GU5KzF8xKb4T34pFyj_imV-wHOjoBcYkc3MCOnIdirM"/>
+      <Avatar size='4xs' src=""/>
       <p>{greeting}</p>
       <Heading>
         {bio1}
